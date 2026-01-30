ea323251081253eab57ec88c5509500bb03e876d
1029yugo-gif <1029.yugo@gmail.com>
2026-01-30 16:41:14 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "フェアリー タスク管理システム",
+    description: "フェアリー（大宮）のタスク管理システム",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+          <html lang="ja">
+                <body>{children}</body>body>
+          </html>html>
+        );
+}</html>
