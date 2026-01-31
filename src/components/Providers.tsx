1a045ae706303b4e45a8f699a20dcc1bef8a8bb6
1029yugo-gif <1029.yugo@gmail.com>
2026-01-31 10:22:41 +0900

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,15 +4,15 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import AuthGuard from '@/components/AuthGuard'
 
 interface ProvidersProps {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
-    return (
-          <AuthProvider>
-                <AuthGuard>
-                  {children}
-                </AuthGuard>AuthGuard>
-          </AuthProvider>AuthProvider>
-        )
-}</AuthProvider>
+  return (
+    <AuthProvider>
+      <AuthGuard>
+        {children}
+      </AuthGuard>
+    </AuthProvider>
+  )
+}
